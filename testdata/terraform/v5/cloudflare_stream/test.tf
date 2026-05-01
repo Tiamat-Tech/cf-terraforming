0c1876b5db0d8b5e9c77818fd43f7b5bfc37d179
Vaishak Dinesh <vaishak@cloudflare.com>
2026-05-01 10:32:22 -0700

    fix: update V5 test fixtures for provider 5.19.1 schema changes
    - cloudflare_observatory_scheduled_test: add frequency and region fields
    - cloudflare_workers_custom_domain: add zone_name field
    - cloudflare_stream: add uid field
    - cloudflare_stream_watermark: remove file field (no longer in schema)

--- testdata/terraform/v5/cloudflare_stream/test.tf
+++ testdata/terraform/v5/cloudflare_stream/test.tf
@@ -1,5 +1,6 @@
 resource "cloudflare_stream" "terraform_managed_resource" {
   account_id = "f037e56e89293a057740de681ac9abbe"
+  uid        = "114a2ce1d6404540ebf02d6dae9fe867"
   meta = jsonencode({
     filename     = "Screenshot 2025-02-19 at 7.57.08\u202fPM.png"
     filetype     = "image/png"
@@ -8,4 +9,3 @@ resource "cloudflare_stream" "terraform_managed_resource" {
     type         = "image/png"
   })
 }
-
